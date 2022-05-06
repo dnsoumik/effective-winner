23b96e14b3a734d1ff50f5942c1ad814f72b8033
Soumik Debnath <soumik.debnath@menlosecurity.com>
2022-05-06 21:15:17 +0530

    Godaddy DNS record updates version 1

---
+++ go_daddy_dns_record_updater.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+
+# Full package imports
+import sys
+import logging
+import schedule
+import time
+from datetime import datetime
+import os
+import logging
+from logging.handlers import RotatingFileHandler
+
+log_formatter = logging.Formatter('%(asctime)s %(levelname)s %(funcName)s(%(lineno)d) %(message)s')
+
+os.system('mkdir -p ./log')
+logFile = './log/dns_scheduler.log'
+
+my_handler = RotatingFileHandler(logFile, mode='a', maxBytes=5*1024*1024,
+                                 backupCount=2, encoding=None, delay=0)
+my_handler.setFormatter(log_formatter)
+my_handler.setLevel(logging.INFO)
+
+log = logging.getLogger('root')
+log.setLevel(logging.INFO)
+
+log.addHandler(my_handler)
+
+import pif
+# Partial imports
+from godaddypy import Client, Account
+
+domain = '<your_domain_name>'
+a_record = 'www'
+
+PUB_KEY = ''
+SEC_KEY = ''
+
+userAccount = Account(api_key=PUB_KEY, api_secret=SEC_KEY)
+userClient = Client(userAccount)
+publicIP = pif.get_public_ip('v4.ident.me')
+
+def update_dns_record():
+    log.info('Current ipv4: ' + publicIP)
+    try:
+        records = userClient.get_records(domain, name=a_record, record_type='A')
+        for record in records:
+            if publicIP != record["data"]:
+                updateResult = userClient.update_record_ip(publicIP, domain, a_record, 'A')
+                if updateResult is True:
+                    log.info('Update ended with no Exception.')
+            else:
+                log.info('No DNS update needed.')
+    except:
+        log.error(sys.exc_info()[1])
+        sys.exit()
+
+def job():
+    now = datetime.now()
+    date_time = now.strftime("%m/%d/%Y, %h:%M:%S")
+    log.info("Schedule started")
+    update_dns_record()
+
+
+schedule.every(10).seconds.do(job)
+log.info("Main service started")
+while True:
+    schedule.run_pending()
+    time.sleep(1)
