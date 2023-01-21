eace60cab46cf608ce521bb995876397a4c6c73b
Soumik Debnath <soumik@xlayer.in>
2023-01-21 11:42:42 +0530

    Fix 4

--- go_daddy_dns_record_updater.py
+++ go_daddy_dns_record_updater.py
@@ -29,32 +29,36 @@ import pif
 # Partial imports
 from godaddypy import Client, Account
 
-domain = '<your_domain_name>'
-a_record = 'www'
+myDomain = '<DOMAIN>'
+myRecord = '<SUB DOMAIN>'
 
 PUB_KEY = ''
 SEC_KEY = ''
 
+recordType = 'A'
+
 userAccount = Account(api_key=PUB_KEY, api_secret=SEC_KEY)
 userClient = Client(userAccount)
-publicIP = pif.get_public_ip('v4.ident.me')
+publicIP = 'IP ADDR'
 
 def update_dns_record():
     log.info('Current ipv4: ' + publicIP)
     try:
-        records = userClient.get_records(domain, name=a_record, record_type='A')
+        records = userClient.get_records(myDomain, name=myRecord, record_type=recordType)
         log.info(records)
+        # us = userClient.delete_records(myDomain, name=myRecord)
+        # log.info(us)
         if len(records):
             for record in records:
                 if publicIP != record["data"]:
-                    updateResult = userClient.update_record_ip(publicIP, domain, a_record, 'A')
+                    updateResult = userClient.update_record_ip(publicIP, myDomain, myRecord, recordType)
                     if updateResult is True:
                         log.info('Update ended with no Exception.')
                 else:
                     log.info('No DNS update needed.')
         else:
-            userClient.add_record(domain, {'data':publicIP,'name':a_record,'ttl': 600, 'type':'A'})
-            log.info('DNS created with no Exception ' + a_record + '.' + domain)
+            userClient.add_record(myDomain, {'data':publicIP,'name':myRecord,'ttl': 600, 'type':recordType})
+            log.info('DNS created with no Exception ' + myRecord + '.' + myDomain)
     except:
         log.error(sys.exc_info()[1])
         sys.exit()
@@ -66,8 +70,8 @@ def job():
     update_dns_record()
 
 
-schedule.every(10).seconds.do(job)
+schedule.every(5).seconds.do(job)
 log.info("Main service started")
 while True:
     schedule.run_pending()
-    time.sleep(1)
+    # time.sleep(0)
